aa9ac42805908dd49d58abfc28bc8242b3b5e5af
“林杰荣” <743568539@qq.com>
2023-04-13 11:01:12 +0800

    修改了配置文件/服务器信息/新增服务器配置文件，修改登录监听信息

--- src/myaxios.js
+++ src/myaxios.js
@@ -1,14 +1,12 @@
 import axios from "axios";
 import Vue from "vue";
-import myconfig from "../public/front.conf"
+import myconf from "../public/front.conf"
 // axios.defaults.baseURL= "http://localhost:8080";
 axios.defaults.withCredentials=true
-axios.defaults.baseURL=myconfig.passengerServer;
+axios.defaults.baseURL=myconf.passengerServer;
 
 // axios.defaults.transformResponse=(data)=>{
 //     return "this is a 测试"
 // }
-
-
 
 Vue.prototype.$axios = axios
